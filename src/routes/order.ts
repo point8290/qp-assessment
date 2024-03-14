a9a8bf5a8f4893a9f8f3d745897d89865c96f101
Krishna Meghwal <krishnameghwal635@gmail.com>
2024-03-14 19:34:53 +0530

    Initial commit

--- src/routes/order.ts
+++ src/routes/order.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { order, getOrder, cancelOrder } from "../controller/orderController";
+import { validateToken } from "../middleware/authorization";
 
 const router = Router();
 
-router.post("/", order);
-router.get("/:id", getOrder);
-router.get("/cancel-order/:id", cancelOrder);
+router.post("/", validateToken, order);
+router.get("/:id", validateToken, getOrder);
+router.get("/cancel-order/:id", validateToken, cancelOrder);
 
 export default router;
